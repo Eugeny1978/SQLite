0af7df491e3a258c58aae7d784f8641f1b078dc9
Eugeny1978 <luchnik78@mail.ru>
2023-10-20 11:24:03 +0300

    Add video_06

---
+++ video_06.py
@@ -0,0 +1,16 @@
+#6: Python SQLite оператор JOIN для формирования сводного отчета
+
+# JOIN <table_name> ON <условия связывания>
+
+# SELECT name, sex, games.score FROM games
+# JOIN users ON games.user_id = users.rowid
+
+# -- SELECT name, sex, games.score FROM games
+# -- -- JOIN users ON games.user_id = users.rowid
+# -- -- INNER JOIN users ON games.user_id = users.rowid
+# -- LEFT JOIN users ON games.user_id = users.rowid
+#
+# SELECT games.user_id, users.name, users.sex, sum(games.score) as sum_scores FROM games
+# LEFT JOIN users ON games.user_id == users.rowid
+# GROUP BY games.user_id
+# ORDER BY sum_scores DESC
